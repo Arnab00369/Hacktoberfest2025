c22f70c62682a0266e314854b86c72d0075139c8
Arnab Jana <arnab003jn.369@gmail.com>
2025-10-02 20:30:09 +0530

    Create Java_Basic.java

---
+++ Java/Java_Basic.java
@@ -0,0 +1,12 @@
+import java.lang.*; //importing package
+
+//***NOTE(V.V.Important):: 
+// Class name and File Name in JAVA should always be EXACTLY SAME
+public class Program_1 //defining class
+{ 
+    public static void main(String args[])//defining main function
+    {
+        //Display or printing message
+        System.out.println("Welcome to the First Java Program!!!");
+    }
+}
